8855f42917840c92a93ce007978696bcfff7fa3e
DNLR <vivaladan@gmail.com>
2017-03-26 11:23:52 +0100

    Added planet texture

--- TriangleWars/TriangleWarsGame.cs
+++ TriangleWars/TriangleWarsGame.cs
@@ -21,7 +21,7 @@ namespace TriangleWars
             //this.World.Scene.Add(new Sprite(0, 0, "Dark-Wood-Texture.jpg"));
             //this.World.Scene.Add(new Sprite(2.0f, 2.0f, "Grass.jpg"));
             //this.World.Scene.Add(new Sprite(0.0f, 0.0f, "runesurprised.png", 0.8f));
-            this.World.Scene.Add(new Circle(0f, 0f, "runesurprised.png", 1));
+            this.World.Scene.Add(new Circle(0f, 0f, "planet1.png", 1));
 
 
         }
